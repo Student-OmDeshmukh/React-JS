ad4acee1fbe74ca68ace8d2325baf5ceb0953573
Student-OmDeshmukh <omdeshmukh@example.com>
2026-02-15 11:17:48 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+ 
+     const heading=React.createElement(
+      "h1",
+      {id:"heading"},
+      "HI I AM A REACT APP");
+     const root=ReactDOM.createRoot(document.getElementById("root"));
+     root.render(heading);
